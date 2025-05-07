74ed6f0dbd61522904c22a6cab9af2a018e23008
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2025-05-07 10:14:01 -0300

    feat: update alpha nvim config

--- .config/nvim/lua/custom/plugins/alpha.lua
+++ .config/nvim/lua/custom/plugins/alpha.lua
@@ -32,8 +32,20 @@ return {
       [[       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ]],
       [[                                   ]],
     }
-    -- Set header
+    local function footer()
+      local total_plugins = #require('lazy').plugins()
+      local datetime = os.date ' %d-%m-%Y   %H:%M:%S'
+      local version = vim.version()
+      local nvim_version_info = '   v' .. version.major .. '.' .. version.minor .. '.' .. version.patch
+
+      -- return datetime .. '   ' .. total_plugins .. ' plugins' .. nvim_version_info
+      return datetime .. nvim_version_info
+    end
+
     dashboard.section.header.val = Hydra
+    dashboard.section.buttons.val = {}
+    dashboard.section.footer.val = footer()
+    dashboard.section.footer.opts.hl = 'Constant'
     require('alpha').setup(dashboard.opts)
   end,
 }
