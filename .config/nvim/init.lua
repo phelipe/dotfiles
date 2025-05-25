05011a79820d2019fb162f88ad1409130041c9ac
phelipe <phelipewesleydeoliveira@gmail.com>
2025-05-25 16:47:37 -0300

    feat: add border to all float windows

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,7 +15,8 @@ vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
-
+-- borders in all float windows
+vim.o.winborder = 'rounded'
 -- disable ~ in empty lines of buffer
 vim.opt.fillchars = { eob = ' ' }
 
