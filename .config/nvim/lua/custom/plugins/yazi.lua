1311d825ce528123ef350472a14b20b332e111cb
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2025-08-20 13:58:26 -0300

    feat: enable yazi tabs

--- .config/nvim/lua/custom/plugins/yazi.lua
+++ .config/nvim/lua/custom/plugins/yazi.lua
@@ -26,6 +26,7 @@ return {
   opts = {
     -- if you want to open yazi instead of netrw, see below for more info
     open_for_directories = false,
+    open_multiple_tabs = true,
     keymaps = {
       show_help = '<f1>',
     },
