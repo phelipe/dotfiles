4dc0f317e51f461bf57a5068f1e278db0c8108a9
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2025-08-22 18:39:24 -0300

    feat: update search shortcuts

--- .config/nvim/lua/custom/plugins/fzf.lua
+++ .config/nvim/lua/custom/plugins/fzf.lua
@@ -26,12 +26,16 @@ return {
     { '<leader>sg', '<cmd>lua require("fzf-lua").grep_project()<cr>', desc = 'Search in Files with Grep' },
     { '<leader>sG', '<cmd>lua require("fzf-lua").grep_curbuf()<cr>', desc = 'Search in Buffer with Grep' },
     -- LSP
-    { 'gr', '<cmd>lua require("fzf-lua").lsp_references()<cr>', desc = '[G]oto [R]eferences' },
-    { 'gd', '<cmd>lua require("fzf-lua").lsp_definitions()<cr>', desc = 'G]oto [D]efinitions' },
-    { 'gI', '<cmd>lua require("fzf-lua").lsp_implementations()<cr>', desc = '[G]oto [I]mplementation' },
-    { '<leader>ss', '<cmd>lua require("fzf-lua").lsp_document_symbols()<cr>', desc = '[D]ocument [S]ymbols' },
-    { '<leader>sS', '<cmd>lua require("fzf-lua").lsp_workspace_symbols()<cr>', desc = '[W]orkspace [S]ymbols' },
-    { '<leader>sD', '<cmd>lua require("fzf-lua").lsp_document_diagnostics()<cr>', desc = '[s]earch [d]iagnostics in file' },
+    { 'gr', '<cmd>lua require("fzf-lua").lsp_references()<cr>', desc = '[g]oto [r]eferences' },
+    { 'gd', '<cmd>lua require("fzf-lua").lsp_definitions()<cr>', desc = '[g]oto [d]efinitions' },
+    { 'gI', '<cmd>lua require("fzf-lua").lsp_implementations()<cr>', desc = '[g]oto [i]mplementation' },
+    { '<leader>ss', '<cmd>lua require("fzf-lua").lsp_document_symbols()<cr>', desc = '[s]search [s]ymbols in buffer' },
+    { '<leader>sS', '<cmd>lua require("fzf-lua").lsp_workspace_symbols()<cr>', desc = '[s]earch [S]ymbols in workspace' },
+    { '<leader>sD', '<cmd>lua require("fzf-lua").lsp_document_diagnostics()<cr>', desc = '[s]earch [D]iagnostics in buffer' },
     { '<leader>sd', '<cmd>lua require("fzf-lua").lsp_workspace_diagnostics()<cr>', desc = '[s]earch [d]iagnostics in workspace' },
+    -- GIT
+    { '<leader>sc', '<cmd>lua require("fzf-lua").git_commits()<cr>', desc = '[s]earch [c]ommits on workspace' },
+    { '<leader>sC', '<cmd>lua require("fzf-lua").git_bcommits()<cr>', desc = '[s]earch [C]ommits on buffer' },
+    { '<leader>sgd', '<cmd>lua require("fzf-lua").git_diff()<cr>', desc = '[s]earch [g]it [d]iff' },
   },
 }
