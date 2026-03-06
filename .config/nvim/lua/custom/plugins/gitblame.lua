70e4439c6a3997e722c428ca4e2fd0b1b8bef268
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2026-03-05 21:20:51 -0300

    update packages

--- .config/nvim/lua/custom/plugins/gitblame.lua
+++ .config/nvim/lua/custom/plugins/gitblame.lua
@@ -3,6 +3,7 @@ return {
   event = { 'BufReadPre' },
   init = function()
     vim.g.gitblame_enabled = 0
+    vim.keymap.set('n', '<leader>gb', '<cmd>GitBlameToggle<cr>', { desc = 'Git Blame toggle' })
   end,
   -- config = function()
   --   require('gitblame').setup {
